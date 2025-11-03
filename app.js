92ecb6de0113d9866ff60060838979de473d0f48
akshayapatel15 <akshaya.gandla15@gmail.com>
2025-11-03 06:32:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3001;
 
+app.get('/', (req, res) => {
+  res.send('Hello from Kubernetes Node.js app!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
